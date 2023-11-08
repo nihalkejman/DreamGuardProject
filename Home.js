38e2dbe6b64702ebdfdc66fee4af87af23f36999
Riaz <riaz.lds@gmail.com>
2023-11-08 02:44:21 +0000

    new changes to home

--- Home.js
+++ Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, ScrollView, Button, TextInput } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Button } from 'react-native';
 
 const HomeScreen = () => {
     const [name, setName] = useState();
@@ -8,45 +8,33 @@ const HomeScreen = () => {
 
     return (
         <ScrollView contentContainerStyle={styles.container}>
-        <Button 
-                title="Start Session" 
+            <Button
+                title="Start Session"
                 onPress={() => alert('Session started.')}
             />
-            <Text style={styles.title}>Emergency contact name:</Text>
-            <TextInput 
-                style={styles.contentInput} 
-                onChangeText={text => setName(text)}
-                value={name}
-                placeholder="Enter emergency contact name"
 
-                
-            />
+            <View style={styles.squareContainer}>
+                <View style={styles.square}>
+                    <Text style={styles.squareText}>10</Text>
+                    <Text style={styles.mphText}>mph</Text>
+                    <Text style={styles.currentSpeedText}>Current Speed</Text>
+                </View>
+            </View>
 
-<Text style={styles.title}>Emergency contact number:</Text>
-            <TextInput 
-                style={styles.contentInput} 
-                onChangeText={text => setNumber(text)}
-                value={number}
-                placeholder="Enter emergency contact number"
+            <Text style={styles.lifetimeStatsText}>Lifetime Stats</Text>
 
-                />
-
-<Text style={styles.title}>Auto Crash Message:</Text>
-            <TextInput 
-                style={styles.contentInput} 
-                onChangeText={text => setMessage(text)}
-                value={message}
-                placeholder="Enter auto crash message"
-
-                />
-       
-
-            {/* Rest of the content */}
-
-            <Button 
-                title="Save Changes" 
-                onPress={() => alert('Emergency settings saved successfully.')}
-            />
+            <View style={styles.statsContainer}>
+                <View style={styles.statsSquare}>
+                    <Text style={styles.squareText}>67</Text>
+                    <Text style={styles.milesText}>miles</Text>
+                    <Text style={styles.totalDistanceText}>Total Distance</Text>
+                </View>
+                <View style={styles.statsSquare}>
+                    <Text style={styles.squareText}>20</Text>
+                    <Text style={styles.mphText}>mph</Text>
+                    <Text style={styles.averageSpeedText}>Average Speed</Text>
+                </View>
+            </View>
         </ScrollView>
     );
 };
@@ -59,26 +47,71 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
         backgroundColor: '#E7EEF6',
     },
-    header: {
+    squareContainer: {
+        alignItems: 'center',
+    },
+    square: {
+        width: 100,
+        height: 100,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 20,
+        borderRadius: 15,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 5,
+    },
+    squareText: {
         fontSize: 24,
-        marginBottom: 10,
-        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+    mphText: {
+        fontSize: 12,
+        color: 'grey',
+    },
+    milesText: {
+        fontSize: 12,
+        color: 'grey',
+    },
+    currentSpeedText: {
+        fontSize: 14,
+        color: 'black',
+        marginTop: 5,
+    },
+    totalDistanceText: {
+        fontSize: 14,
+        color: 'black',
+        marginTop: 5,
+    },
+    averageSpeedText: {
+        fontSize: 14,
+        color: 'black',
+        marginTop: 5,
+    },
+    lifetimeStatsText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginTop: 20,
+    },
+    statsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
         marginTop: 10,
     },
-    title: {
-        fontSize: 18,
-        marginBottom: 5,
-    },
-    content: {
-        fontSize: 16,
-        marginBottom: 20,
-    },
-    contentInput: {
-        borderWidth: 1,
-        borderColor: 'grey',
-        padding: 10,
-        width: '100%',
-        marginBottom: 10,
+    statsSquare: {
+        width: 100,
+        height: 100,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        borderRadius: 15,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 5,
     },
 });
 
