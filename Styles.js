818a45fd386f31ac4fd2fe5c18002bf1ac65c8fd
Riaz <riaz.lds@gmail.com>
2023-11-14 14:03:42 +0000

    latest

---
+++ Styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+const sharedStyles = StyleSheet.create({
+  title: {
+    fontSize: 18,
+    marginBottom: 5,
+  },
+  contentInput: {
+    borderWidth: 1,
+    borderColor: 'grey',
+    padding: 10,
+    width: '100%',
+    marginBottom: 10,
+  },
+});
+
+export default sharedStyles;
